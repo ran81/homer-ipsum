eb8480e625d836e1dd49f8894028b0f7c49e5501
ran81 <ran@gizra.com>
2018-12-31 05:07:45 +0800

    Updates

---
+++ precache-manifest.de15b08a4f64829bc013bbe448f14740.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "14e87e687c6d832eb654103def15c4b2",
+    "url": "/homer-ipsum/static/media/homer.14e87e68.png"
+  },
+  {
+    "revision": "c3eb422708f7d7691f985f8541945197",
+    "url": "/homer-ipsum/static/media/donut.c3eb4227.png"
+  },
+  {
+    "revision": "9c4cdea6ba85c4ca1f456f2fb1abc70c",
+    "url": "/homer-ipsum/static/media/bg1.9c4cdea6.jpg"
+  },
+  {
+    "revision": "48497e4aecdab5c56f2678cd43df541f",
+    "url": "/homer-ipsum/static/media/Homer_Simpson_Revised.48497e4a.ttf"
+  },
+  {
+    "revision": "fea68d97fbd8a3ea93af",
+    "url": "/homer-ipsum/static/js/runtime~main.fea68d97.js"
+  },
+  {
+    "revision": "cb8a08b3a37a0c18e673",
+    "url": "/homer-ipsum/static/js/main.cb8a08b3.chunk.js"
+  },
+  {
+    "revision": "62262cb529d10fc5f289",
+    "url": "/homer-ipsum/static/js/1.62262cb5.chunk.js"
+  },
+  {
+    "revision": "cb8a08b3a37a0c18e673",
+    "url": "/homer-ipsum/static/css/main.52dfc1a2.chunk.css"
+  },
+  {
+    "revision": "62262cb529d10fc5f289",
+    "url": "/homer-ipsum/static/css/1.472c2345.chunk.css"
+  },
+  {
+    "revision": "31e98956b7e0032f34b4128855afcccb",
+    "url": "/homer-ipsum/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f93d85924311e434f6ff364ab054adc.js"
+  "/homer-ipsum/precache-manifest.de15b08a4f64829bc013bbe448f14740.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/homer-ipsum/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
