775d8190d412b991990c06c25985ff4bbdeeca0e
ran81 <ran@gizra.com>
2018-12-30 21:56:29 +0800

    Updates

---
+++ precache-manifest.9152a0e7d4bf9583987db7ef521fc683.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "14e87e687c6d832eb654103def15c4b2",
+    "url": "/homer-ipsum/static/media/homer.14e87e68.png"
+  },
+  {
+    "revision": "c3eb422708f7d7691f985f8541945197",
+    "url": "/homer-ipsum/static/media/donut.c3eb4227.png"
+  },
+  {
+    "revision": "48497e4aecdab5c56f2678cd43df541f",
+    "url": "/homer-ipsum/static/media/Homer_Simpson_Revised.48497e4a.ttf"
+  },
+  {
+    "revision": "fea68d97fbd8a3ea93af",
+    "url": "/homer-ipsum/static/js/runtime~main.fea68d97.js"
+  },
+  {
+    "revision": "a3ebfb63f60c4291a7f2",
+    "url": "/homer-ipsum/static/js/main.a3ebfb63.chunk.js"
+  },
+  {
+    "revision": "62262cb529d10fc5f289",
+    "url": "/homer-ipsum/static/js/1.62262cb5.chunk.js"
+  },
+  {
+    "revision": "a3ebfb63f60c4291a7f2",
+    "url": "/homer-ipsum/static/css/main.e0b642d1.chunk.css"
+  },
+  {
+    "revision": "62262cb529d10fc5f289",
+    "url": "/homer-ipsum/static/css/1.472c2345.chunk.css"
+  },
+  {
+    "revision": "06c1eba56fcb2cb6d919779334b3a6ad",
+    "url": "/homer-ipsum/index.html"
+  }
+];
